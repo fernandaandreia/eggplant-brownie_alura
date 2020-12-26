5641b8bebf6e6a20b7098d47d83710c769a2743b
fernandaandreia <fernanda.nascimento.btk@gmail.com>
2020-12-26 12:51:41 -0300

    invocando-delegates

--- eggplant-brownie_alura-develop/eggplant-brownie/Controllers/AdicionarItensViewController.swift
+++ eggplant-brownie_alura-develop/eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class AdicionarItensViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var caloriasTextField: UITextField!
+    
     
     // MARK: - View life cycle - cuida do ciclo de vida
     override func viewDidLoad() {
@@ -23,7 +28,17 @@ class AdicionarItensViewController: UIViewController {
         //navegar para proxima tela: navigationController.push()
         //navegar para a tela anterior: navigationController.pop()
         
-        navigationController?.popViewController(animated: true)
+        guard let nome = nomeTextField.text, let calorias = caloriasTextField.text else{
+            return
+        }
+        
+        if let numeroDeCalorias = Double(calorias) {
+            let item = Item(nome: nome, calorias: numeroDeCalorias)
+            
+            navigationController?.popViewController(animated: true)
+        }
+        
+        
     }
     
 }
